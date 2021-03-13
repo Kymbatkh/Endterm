89f6ac44810b86e174277aaa1c7ea4d57171b207
Kymbatkh <77831514+Kymbatkh@users.noreply.github.com>
2021-03-13 12:24:58 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+
+http
+  .createServer(function (request, response) {
+    console.log("request ", request.url);
+
+    var filePath = "." + request.url;
+    if (filePath == "./") {
+      filePath = "./index.html";
+    }
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+
+    if (!extname) {
+      filePath += ".html";
+      extname = ".html";
+    }
+
+    var contentType = "text/html";
+    var mimeTypes = {
+      ".html": "text/html",
+      ".js": "text/javascript",
+      ".css": "text/css",
+      ".json": "application/json",
+      ".png": "image/png",
+      ".jpg": "image/jpg",
+      ".mp4": "video/mp4",
+    };
+
+    contentType = mimeTypes[extname] || "application/octet-stream";
+
+    fs.readFile(filePath, function (error, content) {
+      if (error) {
+        if (error.code == "ENOENT") {
+          fs.readFile("./error.html", function (error, content) {
+            response.writeHead(200, { "Content-Type": contentType });
+            response.end(content, "utf-8");
+          });
+        } else {
+          response.writeHead(500);
+          response.end(
+            "Sorry, check with the site admin for error: " +
+              error.code +
+              " ..\n"
+          );
+          response.end();
+        }
+      } else {
+        response.writeHead(200, { "Content-Type": contentType });
+        response.end(content, "utf-8");
+      }
+    });
+  })
+  .listen(8125);
+console.log("Server running at http://127.0.0.1:8125/");
